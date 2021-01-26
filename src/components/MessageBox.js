c9c3ebd243cbfe37ffc27beb87d8ca8c0810a947
Rob <robxrd@gmail.com>
2021-01-26 01:48:20 -0300

    .

---
+++ src/components/MessageBox.js
@@ -0,0 +1,12 @@
+import Quote from "./Quote";
+
+function MessageBox() {
+
+    return (
+        <div className="message-container">
+            <Quote />
+        </div>
+    )
+}
+
+export default MessageBox
---
+++ src/components/Quote.js
@@ -0,0 +1,13 @@
+function Quote() {
+
+    
+
+    return(
+        <div className="quote">
+            <h3>Note that the development build is not optimized.
+                    To create a production build, use npm run build.</h3>
+        </div>
+    )
+}
+
+export default Quote
