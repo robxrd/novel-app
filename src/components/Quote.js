2d503152739a5b1a12057ba723057988bd0f3d45
Rob <robxrd@gmail.com>
2021-01-26 02:52:54 -0300

    save progress

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,11 +1,10 @@
+import story from "../story.json"
+
 function Quote() {
-
     
-
     return(
         <div className="quote">
-            <h3>Note that the development build is not optimized.
-                    To create a production build, use npm run build.</h3>
+            {story[1]}
         </div>
     )
 }
