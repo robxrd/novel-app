d2e8609527a5dad636705e9f75f84bb8d348b2cb
Rob <robxrd@gmail.com>
2021-02-05 19:14:47 -0300

    .

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -1,15 +1,37 @@
 import * as React from 'react'
 import story from "../story.json"
+import song from '../assets/Ketsa - Astral-Travel.mp3'
 
 const {useState} = React
 
 export default function MessageBox() {
-    const [i, nextQuote] = useState(1)
+    let [i, nextQuote] = useState(1)
+    let audio = new Audio(song)
 
     return (
         <div className="master-container">
-            <div className="previous-button" onClick={() => nextQuote(i - 1)}></div>
-            <div className="next-button" onClick={() => nextQuote(i + 1)}></div>
+            <div className="previous-button" onClick={() => {
+                if(i <= 1){
+                    i = 1
+                } else {
+                    nextQuote(i - 1)
+                    }
+                }
+            }></div>
+            <div className="next-button" onClick={() => {
+                if(i === 1){
+                    audio.play()
+                }
+                
+                
+                
+                if(i >= 58) {
+                    i = 58
+                } else {
+                    nextQuote(i + 1)
+                    }
+                }
+            }></div>
             <div className="quote-div">
                 <h3 className="quote">{story[i]}</h3>
             </div>
